5f4e16a7e821ba97dcc538a37d6ac73de340ab62
Muhammad Rifqi Setiawan <muhammadrifqi142@gmail.com>
2024-09-12 16:05:53 +0700

    fix: path issues

--- Phincon_Week_1/day-4/calculator/index.js
+++ Phincon_Week_1/day-4/calculator/index.js
@@ -4,10 +4,10 @@ function toggleTheme() {
   let themeIcon = document.getElementById("theme");
 
   if (themeIcon.src.includes("dark-mode.svg")) {
-    themeIcon.src = "/Phincon_Week_1/day-4/calculator/assets/light-mode.svg";
-    styleCss.href = "/Phincon_Week_1/day-4/calculator/style2.css";
+    themeIcon.src = "Phincon_Week_1/day-4/calculator/assets/light-mode.svg";
+    styleCss.href = "Phincon_Week_1/day-4/calculator/style2.css";
   } else {
-    themeIcon.src = "/Phincon_Week_1/day-4/calculator/assets/dark-mode.svg";
-    styleCss.href = "/Phincon_Week_1/day-4/calculator/style.css";
+    themeIcon.src = "Phincon_Week_1/day-4/calculator/assets/dark-mode.svg";
+    styleCss.href = "Phincon_Week_1/day-4/calculator/style.css";
   }
 }
