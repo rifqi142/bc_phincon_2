b6157badb87963d25dd3ab7bb48b05eb80cccf26
Muhammad Rifqi Setiawan <muhammadrifqi142@gmail.com>
2024-10-08 09:20:43 +0700

    add: gitignore

--- Phicnok_Week_2/day-2/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const barsIcon = document.getElementById("bars-icon");
-  const menu = document.getElementById("menu");
-
-  barsIcon.addEventListener("click", function () {
-    menu.classList.toggle("active");
-  });
-});
